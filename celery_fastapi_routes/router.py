d01ca437a009c273f4c8ee3daf6bd6185fe8f2ac
Frost3281 <Vitaliy.Mikhalchenko@bristolcapital.ru>
2025-01-09 17:04:44 +0300

    try to make all routes async

--- celery_fastapi_routes/router.py
+++ celery_fastapi_routes/router.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from fastapi.concurrency import run_in_threadpool
 from fastapi.responses import UJSONResponse
 
 from .app import celery_app_instance
@@ -17,35 +18,36 @@ celery_router = APIRouter(
 
 
 @celery_router.get('/tasks/get/all', response_model=list[CeleryTaskNoParams])
-def get_running_tasks():
+async def get_running_tasks():
     """Получить все запущенные Celery-таски."""
-    return get_running_celery_tasks(celery_app_instance)
+    return await run_in_threadpool(get_running_celery_tasks, celery_app_instance)
 
 
 @celery_router.get('/tasks/get/{task_id}')
-def get_task_state(task_id: str):
+async def get_task_state(task_id: str):
     """Статус таска."""
-    return UJSONResponse(
-        content={'message': get_celery_task_result(task_id, celery_app_instance)},
-    )
+    return {
+        'message': await run_in_threadpool(
+            get_celery_task_result, task_id, celery_app_instance,
+        ),
+    }
 
 
 @celery_router.post('/tasks/kill/all')
-def kill_all_tasks():
+async def kill_all_tasks():
     """Убиваем все таски."""
-    killed_tasks = kill_celery_tasks(celery_app=celery_app_instance)
-    return UJSONResponse(
-        content={'message': 'tasks were killed', 'task_ids': killed_tasks},
+    killed_tasks = await run_in_threadpool(
+        kill_celery_tasks, celery_app=celery_app_instance,
     )
+    return {'message': 'tasks were killed', 'task_ids': killed_tasks}
 
 
 @celery_router.post('/tasks/kill/ids')
-def kill_tasks_by_ids(task_ids: list[str]):
+async def kill_tasks_by_ids(task_ids: list[str]):
     """Убиваем таски по ID."""
-    killed_tasks = kill_celery_tasks(
+    killed_tasks = await run_in_threadpool(
+        kill_celery_tasks,
         celery_app=celery_app_instance,
         task_selector=lambda task: task.id in task_ids,
     )
-    return UJSONResponse(
-        content={'message': 'tasks were killed', 'task_ids': killed_tasks},
-    )
+    return {'message': 'tasks were killed', 'task_ids': killed_tasks}
